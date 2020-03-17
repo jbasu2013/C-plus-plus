58e172693907fcbf4c37e92a1d17a19cdac7a450
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-03-17 14:48:45 +0530

    arithmetic operation

---
+++ cpp_4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=25, b=10, add, sub, mult, div, mod;
+    add=a+b;
+    sub=a-b;
+    mult=a*b;
+    div=a/b;
+    mod=a%b;
+    cout << "Addition is: " << add << endl;
+    cout << "Subtraction is: " << sub << endl;
+    cout << "Multiplication is: " << mult << endl;
+    cout << "division is: " << div << endl;
+    cout << "modulo division is: " << mod << endl;
+}
