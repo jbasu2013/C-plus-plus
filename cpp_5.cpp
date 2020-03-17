01df73ced00e2199d2b9177332c2c7e2dc456fa7
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-03-17 15:15:41 +0530

    relational operation

---
+++ cpp_5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+     int a=5, b=3,g,l,e,ge,le,ne;
+     g= a>b;
+     l= a<b;
+     e= a==b;
+     ge= a<=b;
+     le= a>=b;
+     ne=a!=b;
+
+     cout << "g is: " << g << endl;
+     cout << "l is: " << l << endl;
+     cout << "e is: " << e << endl;
+     cout << "ge is: " << ge << endl;
+     cout << "le is: " << le << endl;
+     cout << "ne is: " << ne << endl;
+ }
