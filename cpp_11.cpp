63681ad026f16b7e6be6246fed93111db6c19192
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-05 22:28:49 +0530

    Comma Operator

---
+++ cpp_11.cpp
@@ -0,0 +1,13 @@
+// Comma Operator
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    // int a=10;
+    // int b=20;
+    // int c;
+    // Using comma operator, we can declare variable like--
+    int a=10,b=20,c;
+}
