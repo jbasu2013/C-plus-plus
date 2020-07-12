1a4739e8afd44652b42f8e98ecf52f808256b0c9
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-12 19:37:07 +0530

    Explicit Conversation

---
+++ cpp_13.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=5;
+    float y;
+    y = (float)x/2; /* in explicit conversion the type should be declared */
+    cout << "\n Value of y is: " << y << endl;
+}
