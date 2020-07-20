33c412db0d42bc4a81cc8c4b550c62540bb65f29
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-20 16:49:29 +0530

    Local and global variable

---
+++ cpp_15.cpp
@@ -0,0 +1,13 @@
+//Local and Global variable
+// We can use global variable anywhere in the code but local variable only in the particular function only
+
+#include<iostream>
+using namespace std;
+
+int a=10;
+int main()
+{
+    int b=20;
+    cout << "Value of A: " << a << endl;
+    cout << "Value of B: " << b << endl;
+}
