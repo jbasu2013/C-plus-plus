29490fd44d10c5b5fe694802b2b7956e138be31b
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-20 16:59:17 +0530

    punctuators

--- cpp_14.cpp
+++ cpp_14.cpp
@@ -1,1 +1,12 @@
+// Punctuators : here the brackets, punctuation marks are discussed
+
+#include<iostream>
+using namespace std;
+
+int main() //() indicates function calls
+{
+    //{} indicates the start and end of a compound statement
+    int a=10; // ; indicates statement terminator
+    cout << "Value of A: " << a << endl;
+}
 
