d2e42e4f2ab54b7f2723f1c68b0761cff44eefc5
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-08-12 14:44:00 +0530

    if else statement

---
+++ cpp_18.cpp
@@ -0,0 +1,16 @@
+// if else statement
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=15, b=16;
+    if(a>b)
+    {
+        cout << "a is greater." << endl;
+    }
+    else
+    {
+        cout << "b is greater" << endl;
+    }
+}
