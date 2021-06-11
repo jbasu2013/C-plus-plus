0cc34e4a3a78524695d7779caca632b8b1f7fd94
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-06-12 01:06:48 +0530

    Add commit

---
+++ Sum/sum_of_n_natural_numbers.cpp
@@ -0,0 +1,21 @@
+/* Author : Chaitaly Kundu
+   Date : 12th june 2021 */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum =0, i;
+
+    cout << "Enter the number of terms : " ;
+    cin >> n;
+
+    for(i=1;i<=n;i++)
+    {
+        sum = sum + i;
+    }
+    cout << "Sum is " << sum << endl;
+    return 0;
+}
