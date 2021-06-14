c152295794db95392b686caf51deddf0963861bb
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-06-14 17:50:14 +0530

    Add commit

--- pattern/Pyramid/Floyds_triangle.cpp
+++ pattern/Pyramid/Floyds_triangle.cpp
@@ -7,29 +7,30 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, c=1;
     cout << "Enter the no. of row and column: ";
+
     cin >> n ;  // n = row = col
+
     for(int i=1;i<=n;i++)
-{
-    for(int j=1;j<=i;j++)
     {
-        cout << i << " ";
+        for(int j=1;j<=i;j++)
+        {
+            cout << c++ << " ";
+        }
+        cout << endl ;
     }
-    cout << endl ;
-}
     return 0;
 }
 
 /*  O/P:
 
-Enter the no. of row and column: 6
+Enter the no. of row and column: 5
 1
-2 2
-3 3 3
-4 4 4 4
-5 5 5 5 5
-6 6 6 6 6 6
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
 
 
 */
