f4b1a24dba9d5914a5d391294c8737f09a87f71d
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-06-18 23:42:15 +0530

    Add commit

--- Prime-numbers/prime_numbers_btw_two_numbers.cpp
+++ Prime-numbers/prime_numbers_btw_two_numbers.cpp
@@ -14,13 +14,15 @@ int main(){
 
     for(int i=a;i<=b;i++)
     {
+        if(i<=1)
+            continue;
         int c=0;
-        for(int j=2;j<=sqrt(b);j++)
+        for(int j=2;j<= sqrt(i);j++)
         {
             if(i%j==0)
             {
                 c++;
-                continue;
+                break;
             }
         }
         if(c==0)
