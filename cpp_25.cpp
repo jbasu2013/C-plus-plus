22a652286a8829a8d1c94992d1db6be86406f9a2
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-07-03 18:46:53 +0530

    Add commit

---
+++ cpp_25.cpp
@@ -0,0 +1,21 @@
+/* Author : Chaitaly Kundu
+   Date : 03-07-2021*/
+
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    return a*b;
+}
+
+int main(){
+
+    int x=5,y=6;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    cout << "The product of x and y is " << product(x,y) << endl;
+    return 0;
+}
